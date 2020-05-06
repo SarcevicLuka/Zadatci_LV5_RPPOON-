17f136dd250ad650b4128865e92332715086d6b5
Luka Šarčević <5227796-luka_sarcevic@users.noreply.gitlab.com>
2020-05-06 19:16:05 +0000

    Add new file

---
+++ Product.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1
+{
+    class Product : IShipable
+    {
+        private double price;
+        private double weight;
+        private string description;
+        public Product(string description, double price, double weight)
+        {
+            this.description = description;
+            this.price = price;
+            this.weight = weight;
+        }
+        public double Price { get { return this.price; } }
+        public double Weight { get { return this.weight; } }
+        public string Description(int depth = 0)
+        {
+            return new string(' ', depth) + this.description;
+        }
+    }
+}
