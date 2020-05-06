93a8258b6ea03e0173ee8a483afe15159bcc5098
Luka Šarčević <5227796-luka_sarcevic@users.noreply.gitlab.com>
2020-05-06 19:08:51 +0000

    Add new file

---
+++ ReminderNote.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatci_5__6__7
+{
+    class ReminderNote : Note
+    {
+        public ReminderNote(string message, ITheme theme) : base(message, theme) { }
+        public override void Show()
+        {
+            this.ChangeColor();
+            Console.WriteLine("REMINDER: ");
+            string framedMessage = this.GetFramedMessage();
+            Console.WriteLine(framedMessage);
+            Console.ResetColor();
+        }
+    }
+}
