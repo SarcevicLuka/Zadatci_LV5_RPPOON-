2cdad6bca4736d868c3eedf12d0a59a640d4e26e
Luka Šarčević <5227796-luka_sarcevic@users.noreply.gitlab.com>
2020-05-06 19:08:20 +0000

    Add new file

---
+++ LightTheme.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatci_5__6__7
+{
+    class LightTheme : ITheme
+    {
+        public void SetBackgroundColor()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+        }
+        public void SetFontColor()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        public string GetHeader(int width)
+        {
+            return new string('+', width);
+        }
+        public string GetFooter(int width)
+        {
+            return new string('_', width);
+
+        }
+    }
+}
