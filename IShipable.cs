8253a0aa9be51d2cb76134fb10de981b975ac543
Luka Šarčević <5227796-luka_sarcevic@users.noreply.gitlab.com>
2020-05-06 19:15:37 +0000

    Add new file

---
+++ IShipable.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1
+{
+    interface IShipable
+    {
+        double Price { get; }
+        double Weight { get; }
+        string Description(int depth = 0);
+    }
+}
